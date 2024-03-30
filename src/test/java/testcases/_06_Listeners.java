febd2339f9be305029c7932088b9f2b93ba9370c
Ankur Gupta <agupta@sephora.co.in>
2024-03-30 16:08:49 +0530

    Implemented testng listners

---
+++ src/test/java/testcases/_06_Listeners.java
@@ -0,0 +1,17 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _06_Listeners {
+    
+    @Test
+    public void login() {
+	Assert.fail("Login failed");
+    }
+    
+    @Test
+    public void login1() {
+    }
+
+}
