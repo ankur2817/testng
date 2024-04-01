5f5cd4c733e39e5f95ceca2e4e28be927c8dba9d
Ankur Gupta <agupta@sephora.co.in>
2024-04-01 16:00:55 +0530

    Added the DataProvider case with Excel reading

--- src/test/java/testcases/_07_TestParameterization_DataProvider.java
+++ src/test/java/testcases/_07_TestParameterization_DataProvider.java
@@ -3,10 +3,20 @@ package testcases;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import utility.ExcelReader;
+
 public class _07_TestParameterization_DataProvider {
     
-    @Test(dataProvider="getData")
+    public static ExcelReader excel = null;
+    
+    //@Test(dataProvider="getData")
     public void login(String username, String password) {
+	System.out.println("username is " + username + " and password is " + password);
+	
+    }
+    
+    @Test(dataProvider="getData1")
+    public void login1(String username, String password) {
 	System.out.println("username is " + username + " and password is " + password);
 	
     }
@@ -22,6 +32,25 @@ public class _07_TestParameterization_DataProvider {
 	data[2][0] = "john";
 	data[2][1] = "john123";
 	
+	return data;
+    }
+    
+    @DataProvider
+    public Object[][] getData1() {
+	
+	if(excel == null)
+	    excel = new ExcelReader("./src/test/resources/Book1.xlsx");
+	
+	String sheetName = "Sheet1";
+	int rowCount = excel.getRowCount(sheetName);
+	int columnCount = excel.getColumnCount(sheetName);
+	Object[][] data = new Object[rowCount - 1][columnCount];
+	
+	for(int i = 2; i <= rowCount; i++) {
+	    for(int j = 0; j < columnCount; j++) {
+		data[i-2][j] = excel.getCellData(sheetName, j, i);
+	    }
+	}
 	return data;
     }
 
